5b3e2b11f80e75a4f302aec7ed4984cd73fddc91
jA71 <isjavier77@gmail.com>
2022-06-15 12:32:57 -0500

    Se agrega proyecto para leccion

---
+++ app/src/main/java/com/example/leccionfragmentos/interface_user/Data.kt
@@ -0,0 +1,12 @@
+package com.example.leccionfragmentos.interface_user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.leccionfragmentos.R
+
+class Data : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data)
+    }
+}
